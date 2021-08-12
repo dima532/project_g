62f5731037582a3ece30d59cf5b7472857ce31b9
dima532 <dimondmit@gmail.com>
2021-08-12 19:45:05 +0300

    +pm

--- New Unity Project/Assets/scripts/walking.cs
+++ New Unity Project/Assets/scripts/walking.cs
@@ -9,12 +9,14 @@ public class walking : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer sp;
     private Vector2 sas;
+    private Collider2D col;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sp = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
